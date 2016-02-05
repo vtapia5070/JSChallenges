af0022247977066d166c2a6f99065b023ed7c185
Victoria <tapia.victoria5070@gmail.com>
2016-02-05 09:51:38 -0800

    added fizzBuzz solution

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -15,8 +15,35 @@
 * "Y" is in range [1, 20]
 * "N" is in range [21, 100]
 
+* INPUT SAMPLE:
+
+* Your program should accept a file as its first argument. The file contains multiple 
+* separated lines; each line contains 3 numbers that are space delimited. The first number 
+* is the first divider (X), the second number is the second divider (Y), and the third 
+* number is how far you should count (N). You may assume that the input file is formatted 
+* correctly and the numbers are valid positive integers.
+
+OUTPUT SAMPLE:
+
+* Print out the series 1 through N replacing numbers divisible by X with “F”, numbers 
+* divisible by Y with “B” and numbers divisible by both with “FB”. Since the input file 
+* contains multiple sets of values, your output should print out one line per set. Ensure 
+* that there are no trailing empty spaces in each line you print.
 */
 
-function fizzBuzz (x, y, z) {
-  // create solution here
+function fizzBuzz (x, y, n) {
+  for (var i = 1; i <= n; i++) {
+    if (i % x === 0 && i % y === 0) {
+      console.log("FB");
+    } else if (i % x === 0) {
+      console.log("F");
+    } else if (i % y === 0) {
+      console.log("B")
+    } else {
+      console.log(i);
+    }
+  }
 }
+
+// fizzBuzz(3,5,10); // 1 2 F 4 B F 7 8 F B
+fizzBuzz(2,7,15); // 1 F 3 F 5 F B F 9 F 11 F 13 FB 15
