bfdf0f3ce4081ad7c1e7e7434b8973ecf20d148e
Victoria <tapia.victoria5070@gmail.com>
2016-02-05 09:40:41 -0800

    added fizzBuzz challenge

---
+++ README.md
@@ -0,0 +1,5 @@
+#Algorithms
+
+##Summary
+
+This repo is a collection of Javascript algorithms that will be updated daily with either new problems or solutions on the solution branch.
---
+++ fizzBuzz.js
@@ -0,0 +1,22 @@
+/*
+* Players generally sit in a circle. The first player says the number “1”,
+* and each player says next number in turn. However, any number divisible by X 
+* (for example, three) is replaced by the word fizz, and any divisible by Y (for 
+* example, five) by the word buzz. Numbers divisible by both become fizz buzz. A 
+* player who hesitates, or makes a mistake is eliminated from the game.
+
+* Write a program that prints out the final series of numbers where those divisible
+* by X, Y and both are replaced by “F” for fizz, “B” for buzz and “FB” for fizz buzz.
+
+* CONSTRAINTS:
+
+* The number of test cases ≤ 20
+* "X" is in range [1, 20]
+* "Y" is in range [1, 20]
+* "N" is in range [21, 100]
+
+*/
+
+function fizzBuzz (x, y, z) {
+  // create solution here
+}
