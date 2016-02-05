91f3a9eff07f32b9afee7083ae63f32f95ff3baa
Victoria <tapia.victoria5070@gmail.com>
2016-02-05 09:52:41 -0800

    modified fizzBuzz challenge

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,4 +1,6 @@
 /*
+* (Provided by CodeEval)
+
 * Players generally sit in a circle. The first player says the number “1”,
 * and each player says next number in turn. However, any number divisible by X 
 * (for example, three) is replaced by the word fizz, and any divisible by Y (for 
@@ -15,8 +17,23 @@
 * "Y" is in range [1, 20]
 * "N" is in range [21, 100]
 
+* INPUT SAMPLE:
+
+* Your program should accept a file as its first argument. The file contains multiple 
+* separated lines; each line contains 3 numbers that are space delimited. The first number 
+* is the first divider (X), the second number is the second divider (Y), and the third 
+* number is how far you should count (N). You may assume that the input file is formatted 
+* correctly and the numbers are valid positive integers.
+
+OUTPUT SAMPLE:
+
+* Print out the series 1 through N replacing numbers divisible by X with “F”, numbers 
+* divisible by Y with “B” and numbers divisible by both with “FB”. Since the input file 
+* contains multiple sets of values, your output should print out one line per set. Ensure 
+* that there are no trailing empty spaces in each line you print.
+
 */
 
-function fizzBuzz (x, y, z) {
+function fizzBuzz (x, y, n) {
   // create solution here
 }
