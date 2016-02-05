574cdcbb688c70477f48c4f8734933b3a382d44f
Victoria <tapia.victoria5070@gmail.com>
2016-02-05 10:02:53 -0800

    modified .gitignore commented fizzbuzz test case

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -46,4 +46,4 @@ function fizzBuzz (x, y, n) {
 }
 
 // fizzBuzz(3,5,10); // 1 2 F 4 B F 7 8 F B
-fizzBuzz(2,7,15); // 1 F 3 F 5 F B F 9 F 11 F 13 FB 15
+// fizzBuzz(2,7,15); // 1 F 3 F 5 F B F 9 F 11 F 13 FB 15
