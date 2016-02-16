fb25b5a79f3249e660265c7326b7010b304faf6b
Victoria <tapia.victoria5070@gmail.com>
2016-02-16 10:31:46 -0800

    added timer.js challenge

---
+++ timer.js
@@ -0,0 +1,10 @@
+/*
+Creat a timer function that takes in a number, n, and prints
+n down to 1.
+n should be immediately printed then print the next decremented
+number after one second (1000ms), until 1 is printed.
+*/
+
+var timer = function (n) {
+  // code goes here
+};
