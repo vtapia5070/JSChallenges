67e1d1921a03858ee5c88172a2d513ea61796a35
Victoria <tapia.victoria5070@gmail.com>
2016-02-16 10:33:53 -0800

    timer.js solution

--- timer.js
+++ timer.js
@@ -7,4 +7,15 @@ number after one second (1000ms), until 1 is printed.
 
 var timer = function (n) {
   // code goes here
-}
+  var sec = 0;
+  for (var i = n; i >= 1; i--) {
+    (function (num, ms) {
+      setTimeout(function() {
+        console.log(num);
+      }, ms);
+    })(i, sec);
+    sec+=1000;
+  }
+};
+
+timer(5); // should finish in 4s
