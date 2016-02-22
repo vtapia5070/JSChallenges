4a7cb0e0eeaef2ab96399d4dcf038a7ff16ac25a
Victoria <tapia.victoria5070@gmail.com>
2016-02-21 17:34:40 -0800

    added twoSums coding challenge

---
+++ twoSums.js
@@ -0,0 +1,13 @@
+/*
+Given an array of integers, find two numbers such that they add
+up to a specific target number.
+
+The function twoSum should return indices of the two numbers such
+that they add up to the target, where index1 must be less than index2.
+
+You may assume that each input would have exactly one solution.
+*/
+
+function twoSums (arr, target) {
+  // code here
+}
