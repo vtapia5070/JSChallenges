94824a5d760e68be7ec2e6915de09a2e35da7f44
Victoria <tapia.victoria5070@gmail.com>
2016-02-26 14:59:55 -0800

    modified mergeSort and Hash Table

--- dataStructures/HashTable/hashTable.js
+++ dataStructures/HashTable/hashTable.js
@@ -1,15 +1,6 @@
-/**
- * Create a hash table with `insert()`, `retrieve()`, and `remove()` methods.
- * Be sure to handle hashing collisions correctly.
- * Set your hash table up to double the storage limit as
- * soon as the total number of items stored is greater than
- * 3/4th of the number of slots in the storage array.
- * Resize by half whenever utilization drops below 1/4.
- */
-
-// This is a "hashing function". You don't need to worry about it, just use it
-// to turn any string into an integer that is well-distributed between
-// 0 and max - 1
+/*
+Create a hash table 
+*/
 var getIndexBelowMaxForKey = function(key, storageLimit){
   var hash = 0;
   for (var i = 0; i < str.length; i++) {
