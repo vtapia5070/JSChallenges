084e5924a66a78174a0ce03a6ea8caaac1319381
Victoria <tapia.victoria5070@gmail.com>
2016-03-01 18:50:55 -0800

    modified hashTable.txt added Stack.js

---
+++ dataStructures/Stacks/stacks.js
@@ -0,0 +1,16 @@
+/*
+Create a Stack Data structure in prototypal instantiation.
+*/
+
+var Stack = function () {
+};
+
+var StackMethods = {
+
+  size: function () {},
+
+  push: function (value) {},
+  
+  pop: function () {}
+
+};
