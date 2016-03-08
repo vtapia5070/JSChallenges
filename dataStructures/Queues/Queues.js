bf5d91d4b48532b591122a960b3de4a2b7ea998a
Victoria <tapia.victoria5070@gmail.com>
2016-03-07 19:33:15 -0800

    completed Queue data structure

---
+++ dataStructures/Queues/Queues.js
@@ -0,0 +1,26 @@
+/*
+Create a Queue Data structure in pseudoclassical instantiation.
+*/
+
+var Queue = function () {
+  this._front = 0;
+  this._back = 0;
+  this._storage = {};
+};
+
+Queue.prototype.size: function () {
+  return this._back - this._front;
+};
+
+Queue.prototype.push: function (value) {
+  this._back++;
+  this._storage[this._back] = value;
+};
+
+Queue.prototype.pop: function () {
+  var front = this._storage[this._front];
+  delete this._storage[this._front];
+  this._front++;
+  return front;
+};
+
