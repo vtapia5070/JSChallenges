02614685f2cd2ad3413d5c500f1ebc8379f6a459
Victoria <tapia.victoria5070@gmail.com>
2016-03-07 19:25:32 -0800

    added Queues data structure

---
+++ dataStructures/Queues/Queues.js
@@ -0,0 +1,19 @@
+/*
+Create a Queue Data structure in pseudoclassical instantiation.
+*/
+
+var Stack = function () {
+  this._front = 0;
+  this._back = 0;
+};
+
+var StackMethods = {
+
+  size: function () {
+  },
+
+  push: function (value) {},
+  
+  pop: function () {}
+
+};
