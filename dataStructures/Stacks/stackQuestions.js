dc0e947496c6d930175390c74a82acbf3717a0c4
victoria tapia <victoria_tapia@discovery.com>
2017-04-05 17:05:21 -0700

    add file for cracking the coding interview questions

---
+++ dataStructures/Stacks/stackQuestions.js
@@ -0,0 +1,15 @@
+/*
+How would you use a single array to implement three stacks?
+*/
+
+/*
+How would you implement a stck that also has a min method that returns the minimum element?
+Push, pop, and min should all operate in O(1) time.
+*/
+
+/*
+Implement a datastructure SetOfStacks that mimics a stack that creates a new stack when it's previous stack exceeds some threshold or capacity.
+This SetOfStacks should have a SetOfStacks.push(), SetOfStacks.pop(), and should act identically to a single stack.
+
+** Implement a popAt(index) which performs a pop operation on a set of sub-stacks.
+*/
